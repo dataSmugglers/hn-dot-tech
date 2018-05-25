1b1d6c52822005f3bfd0575f1127b9b7507a77df
hexphase <13786214+hexphase@users.noreply.github.com>
2018-05-25 13:43:42 -0600

    hn get top post json

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -2,11 +2,14 @@
 
 var post = require('../models/post');
 var mongoose = require('mongoose');
+var hn = require('hackernews-api');
 
 module.exports.home = home;
 
 function home (req, res) {
-  
-  res.render('index',  { title: 'hn-dot-tech' });
-};
+    var topPostId = hn.getTopStories(1);
+    var topPost = hn.getItem(topPostId[0]);
+    var string = JSON.stringify(topPost);
+    res.render('index',  { title: string });
+}
 
