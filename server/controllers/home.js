2ceee8be73b5fcd569e785234033901f00fd38e4
sal <sal2993@hotmail.com>
2018-05-28 13:06:39 -0600

    need to update my code

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -6,7 +6,14 @@ var mongoose = require('mongoose');
 module.exports.home = home;
 
 function home (req, res) {
+
+  // database setup
+  var url = "mongodb://localhost:27017/hndb";
+  mongoose.Promise = global.promise;
+  mongoose.connect(url);
+
+  var myData = new Post(
   
   res.render('index',  { title: 'hn-dot-tech' });
-};
+}
 
---
+++ server/hn_api_requests/apiRequests.js
@@ -0,0 +1,4 @@
+'use strict';
+var mongoose = require("mongoose");
+
+console.log("hello");
