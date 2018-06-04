588ff7564d4736fb112fc1ffc960da64cc99d7de
sal <sal2993@hotmail.com>
2018-06-04 09:00:16 -0600

    Made the basic NavBar 'look' work

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(function(err, req, res, next) {
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/hndb";
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, {useNewUrlParser: true}, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
   db.close();
