5b403f8a08114c4e781befdeef6e495666f79aae
sal <sal2993@hotmail.com>
2018-06-07 16:56:00 -0600

    REFACTORED: cleaned up the apiRequests, its much more readable and testable

--- apiRequests/apiRequests.js
+++ apiRequests/apiRequests.js
@@ -49,10 +49,29 @@ const logger = new (winston.Logger) ({
 
 async function main() {
     var goOn = true;
-    var lastTopPost = 0;
+    var lastTopPost = 0; // Im sick of this line. Im changing it to read the latest post
 
     logger.log('info', 'MAIN: main has started');
 
+    // Before loop begins, get the top post
+    var db = connectToDatabase(url);
+    db.once('open', function() {
+
+        var topPostId = getHackerNewsApiRequest_TopPosts();
+        var firstTopPost = getHackerNewsApiRequest_TopPostInfo(topPostId[0]);
+        lastTopPost = topPostId[0];
+
+        if (is_Post_in_hndb(url, topPostId[0])){
+            // Delete and add again with new initTime
+            delete_Post_by_id(url, topPostId[0])
+            add_new_Post(url, firstTopPost.id, firstTopPost.title, firstTopPost.url, firstTopPost.score);
+        }
+        else {
+            // Add to db
+            add_new_Post(url, firstTopPost.id, firstTopPost.title, firstTopPost.url, firstTopPost.score);
+        }
+
+    });
     while(goOn) {
   	// Connect to DB
         var db = connectToDatabase(url);
@@ -62,7 +81,7 @@ async function main() {
 
           var topPostId = getHackerNewsApiRequest_TopPosts();
 
-          var firstTopPost = getHackerNewsApiRequest_TopPostInfo(topPostId);
+          var firstTopPost = getHackerNewsApiRequest_TopPostInfo(topPostId[0]);
           logger.log('info', firstTopPost);
 
           // Make sure that this post is the same top post
@@ -152,8 +171,122 @@ function getHackerNewsApiRequest_TopPosts(){
 }
 
 function getHackerNewsApiRequest_TopPostInfo(topPostId){
-    var firstTopPost = hn.getItem(topPostId[0]);
+    var firstTopPost = hn.getItem(topPostId);
     return firstTopPost;
+}
+
+/*
+* @param db     a valid mongoose db connection
+* @param postId a hackernews post id
+* @return       boolean, whether or not the id is in the db
+ */
+function is_Post_in_hndb(db_url, postId){
+    var db = connectToDatabase(db_url);
+    db.once('open', function(){
+        Post.find({hnid: postId}, function(err, posts){
+            if(err) {
+                logger.log('error', err);
+                return false;
+            }
+            // Found one or more posts
+            if (posts.length) {
+                logger.log('info', 'Found Post with same id, updating init Time');
+                logger.log('info', posts);
+                // TODO: There is some weird error I got with this line:
+                return true;
+            }
+            // Didnt find post in db
+            else{
+                logger.log('info', "Didnt find post in DB, Proceed to insert");
+                return false;
+            }
+            // Did find in DB, don't save it
+        });
+    });
+}
+
+/*
+* @param db     a valid mongoose db connection
+* @param postId a hackernews post id
+* @return       boolean, whether or not the id is in the db
+ */
+function add_to_Post_finalTimeAsTop(db_url, postId) {
+    var db = connectToDatabase(db_url);
+    db.once('open', function() {
+        Post.find({hnid: postId}, function (err, posts) {
+            if (err) {
+                logger.log('error', err);
+                return false;
+            }
+            // Found one or more posts
+            if (posts.length) {
+                logger.log('info', 'Found Post with same id, updating final Time');
+                logger.log('info', posts);
+                // TODO: There is some weird error I got with this line:
+                posts.initTimeAsTop.push(new Date());
+                posts.save(function (err) {
+                    logger.log('error', 'Problem Saving final time')
+                    return false;
+                });
+                return true;
+            }
+        });
+    });
+    return false;
+}
+function add_new_Post(db_url, post_Id, post_title, post_url, post_votes){
+    var db = connectToDatabase(db_url);
+    db.once('open', function() {
+        var myData = new Post({
+            hnid: post_Id, title: post_title,
+            url: post_url, votes: post_votes
+        });
+        myData.initTimeAsTop.push(new Date());
+        myData.save(function (err) {
+            if (err) {
+                return logger.log('error', 'DB: Could not save to db');
+            }
+        });
+    });
+}
+function delete_Post_by_id(db_url, post_id){
+    var db = connectToDatabase(db_url);
+    db.once('open', function() {
+        Post.deleteOne({hnid: post_id}, function(err, posts) {
+            if (err) {
+                logger.log('error', err);
+                return false;
+            }
+            return true;
+        });
+        return false;
+    });
+}
+
+function add_to_Post_initTimeAsTop(db_url, post_id){
+    var db = connectToDatabase(db_url);
+    db.once('open', function(){
+        Post.find({hnid: post_id}, function(err, posts) {
+            if (err) {
+                logger.log('error', err);
+                return false;
+            }
+            // Found one or more posts
+            if (posts.length) {
+                logger.log('info', 'Found Post with same id, updating init Time');
+                logger.log('info', posts);
+                // TODO: There is some weird error I got with this line:
+                posts.initTimeAsTop.push(new Date());
+                posts.save(function (err) {
+                    logger.log('error', 'Problem Saving initTimeAsTop' + err);
+                    return false;
+                });
+                return true;
+            }
+            return false;
+        });
+    });
+
 }
 
 main();
